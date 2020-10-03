b43a0907da4c181d1183ae7e9267f91cbf3b80f9
Frytz <dacook1983@gmail.com>
2020-10-02 21:29:21 -0400

    stretches completed

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,20 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    if end is None:
+        end = len(arr) - 1
 
+    if start > end:
+        return -1
+        
+    mid = (start + end) // 2
+
+    if target is arr[mid]:
+        return mid 
+
+    if target < arr[mid]:
+        return binary_search(arr, target, start, mid - 1)
+
+    return binary_search(arr, target, mid + 1, end)
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -9,6 +22,40 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
+def descending_binary_search(arr, target, left, right):
+    # base case 
+    # or we searched the whole array, i.e. when left > right
+    if left > right:
+        return -1
+    # how do we move closer to a base case? 
+    # we'll stop when we either find the target 
+    else:
+        mid = (left + right) // 2
+        
+        if arr[mid] == target:
+            return mid
+
+        elif arr[mid] < target:
+            # rule out the right side
+            return descending_binary_search(arr, target, left, mid - 1)
+        else:
+            # rule out the left side 
+            return descending_binary_search(arr, target, mid + 1, right)
+
+
 def agnostic_binary_search(arr, target):
     # Your code here
+    # figure out whether the input array is sorted in ascending or descending order 
+    # keep a boolean to indicate the order of the array 
+    # compare arr[0] and arr[1] 
+    if arr[0] > arr[-1]:
+        is_ascending = False
+    else:
+        is_ascending = True
 
+    # if the input array is ascending, call `binary_search` with the array and target 
+    if is_ascending:
+        return binary_search(arr, target, 0, len(arr) - 1)
+    # otherwise, call `descending_binary_search` with the array and target 
+    else:
+        return descending_binary_search(arr, target, 0, len(arr) - 1)
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,16 +4,42 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
+    crawler = 0
+    a = 0
+    b = 0
+    while a + b < elements:
 
+        if a > len(arrA) - 1:
+            merged_arr[crawler] = arrB[b]
+            b += 1
+            crawler += 1
+        elif b > len(arrB) - 1:
+            merged_arr[crawler] = arrA[a]
+            a += 1
+            crawler += 1
+        elif arrA[a] < arrB[b]:
+            merged_arr[crawler] = arrA[a]
+            a += 1
+            crawler += 1
+        else:
+            merged_arr[crawler] = arrB[b]
+            b += 1
+            crawler += 1
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+    elements = len(arr)
+    mid = elements // 2
 
-
-    return arr
+    if mid > 0:
+        arrA = merge_sort(arr[0:mid])
+        arrB = merge_sort(arr[mid:])
+        return merge(arrA, arrB)
+    else:
+        return arr
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
@@ -21,8 +47,41 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    start2 = mid +1
+    #If the direct merge is already sorted
+    if arr[mid] <= arr[start2]:
+        return
+    
+    #Two pointers to maintain start of both array merges
+    while start <= mid and start2 <=end:
+        
+        #if ele 1 is in the right place
+        if arr[start] <= arr[start2]:
+            start += 1
+        
+        else:
+            value = arr[start2]
+            index = start2
+
+            #shift all the ele between ele 1 , ele 2, right by 1
+            while index != start:
+                arr[index] = arr[index - 1]
+                index -= 1
+            arr[start] = value
+
+            #updates all pointers
+            start += 1
+            mid += 1
+            start2 += 1
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+    if l < r:
+        #helps avoid overflow of large l and r
+        m = l + (r - l) // 2
+        #sort first and second halves
+        merge_sort_in_place(arr , l, m)
+        merge_sort_in_place(arr, m + 1, r)
+        merge_in_place(arr, l, m, r)
 
